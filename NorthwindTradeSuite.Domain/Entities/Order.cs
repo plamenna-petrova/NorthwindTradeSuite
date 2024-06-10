8a21771adf3ca6d472ba01e9ff74aa3438e3488a
plamenna-petrova <plamennavp@abv.bg>
2024-06-10 13:08:11 +0300

    Continued building the Domain layer

--- NorthwindTradeSuite.Domain/Entities/Order.cs
+++ NorthwindTradeSuite.Domain/Entities/Order.cs
@@ -9,6 +9,34 @@ namespace NorthwindTradeSuite.Domain.Entities
 {
     public class Order : BaseEntity
     {
+        public string CustomerID { get; set; }
 
+        public virtual Customer Customer { get; set; }
+
+        public string EmployeeID { get; set; }
+
+        public virtual Employee Employee { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        public DateTime? RequiredDate { get; set; }
+
+        public DateTime? ShippedDate { get; set; }
+
+        public int? ShippedVia { get; set; }
+
+        public decimal? Freight { get; set; }
+
+        public string ShipName { get; set; }
+
+        public string ShipAddress { get; set; }
+
+        public string ShipCity { get; set; }
+
+        public string ShipRegion { get; set; }
+
+        public string ShipPostalCode { get; set; }
+
+        public string ShipCountry { get; set; }
     }
 }
