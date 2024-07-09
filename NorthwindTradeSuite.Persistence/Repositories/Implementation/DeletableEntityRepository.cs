6ce0a3bde607bc619de3df8b6be77bb2b294942a
plamenna-petrova <plamennavp@abv.bg>
2024-07-09 19:08:11 +0300

    Small deletable entity repository logic fix

--- NorthwindTradeSuite.Persistence/Repositories/Implementation/DeletableEntityRepository.cs
+++ NorthwindTradeSuite.Persistence/Repositories/Implementation/DeletableEntityRepository.cs
@@ -41,16 +41,16 @@ namespace NorthwindTradeSuite.Persistence.Repositories.Implementation
         public IQueryable<TEntity> GetAllWithDeletedEntities(bool asNoTracking = false) => base.GetAll(asNoTracking).IgnoreQueryFilters();
 
         public async Task<List<TEntity>> GetAllWithOptionalDeletionFlagAsync(bool isDeletedFlag = false, bool asNoTracking = false)
-            => await base.GetAllByConditionAsync(e => e.IsDeleted == isDeletedFlag, asNoTracking);
+            => await GetAllByConditionAsync(e => e.IsDeleted == isDeletedFlag, asNoTracking);
 
         public IQueryable<TEntity> GetByIdWithOptionalDeletionFlagAsQueryable(string id, bool isDeletedFlag = false, bool asNoTracking = false)
             => GetAllByCondition(e => e.Id == id && e.IsDeleted == isDeletedFlag, asNoTracking);
 
         public async Task<TEntity?> GetFirstOrDefaultByIdWithOptionalDeletionFlagAsync(string id, bool isDeletedFlag = false, bool asNoTracking = false)
-            => await base.GetAllByCondition(e => e.IsDeleted == isDeletedFlag, asNoTracking).FirstOrDefaultAsync(e => e.Id == id);
+            => await GetAllByCondition(e => e.IsDeleted == isDeletedFlag, asNoTracking).FirstOrDefaultAsync(e => e.Id == id);
 
         public async Task<TEntity?> GetSingleOrDefaultByIdWithOptionalDeletionFlagAsync(string id, bool isDeletedFlag = false, bool asNoTracking = false)
-            => await base.GetAllByCondition(e => e.IsDeleted == isDeletedFlag, asNoTracking).SingleOrDefaultAsync(e => e.Id == id);
+            => await GetAllByCondition(e => e.IsDeleted == isDeletedFlag, asNoTracking).SingleOrDefaultAsync(e => e.Id == id);
 
         public void HardDelete(TEntity entityToHardDelete) => base.Delete(entityToHardDelete);
 
